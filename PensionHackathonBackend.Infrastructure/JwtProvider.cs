efc15d383899a30c76d2f315f8c67ccf6162fd24
SimpleFreak <ISFreak@yandex.ru>
2024-10-26 00:27:44 +0500

    Добавил хеширование паролей для регистрации и авторизации пользователей в системе.

---
+++ PensionHackathonBackend.Infrastructure/JwtProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using PensionHackathonBackend.Core.Models;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PensionHackathonBackend.Infrastructure
+{
+    public class JwtProvider(IOptions<JwtOptions> options)
+    {
+        private readonly JwtOptions _options = options.Value;
+
+        public string GenerateToken(User user)
+        {
+            Claim[] claims = [new("userId", user.Id.ToString())];
+
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
+                SecurityAlgorithms.HmacSha512);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                signingCredentials: signingCredentials,
+                expires: DateTime.UtcNow.AddHours(_options.ExpiresHours));
+
+            var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenValue;
+        }
+    }
+}
