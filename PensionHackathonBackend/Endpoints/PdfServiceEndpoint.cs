725d2b77d5553d19c69fca3dced030bd6af61854
SimpleFreak <ISFreak@yandex.ru>
2024-10-27 11:37:05 +0500

    Добавил изменения в файл по работе с PDF документами.

--- PensionHackathonBackend/Endpoints/PdfServiceEndpoint.cs
+++ PensionHackathonBackend/Endpoints/PdfServiceEndpoint.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
+
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
 using iText.Layout.Properties;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,45 +15,47 @@ namespace PensionHackathonBackend.Endpoints;
 
 public static class PdfServiceEndpoint
 {
+    /* Добавление методов для отображения запросов по работе с Pdf файлами */
     public static IEndpointRouteBuilder AddPdfServiceEndpoints(this IEndpointRouteBuilder app)
     {
         app.MapPost("GeneratePdf", GeneratePdf);
+
         return app;
     }
 
-    private static async Task<IResult> GeneratePdf([FromBody] List<KeyValuePair<int, string>> data)
+    /* Генерация Pdf файла из табличных данных */
+    private static IResult GeneratePdf([FromBody] List<KeyValuePair<int, string>> data)
     {
-        using (var memoryStream = new MemoryStream())
+        using var memoryStream = new MemoryStream();
+        var pdfWriter = new PdfWriter(memoryStream);
+        var pdfDocument = new PdfDocument(pdfWriter);
+        var document = new Document(pdfDocument);
+
+        document.Add(new Paragraph("Таблица Идентификатор - Значение")
+            .SetTextAlignment(TextAlignment.CENTER)
+            .SetFontSize(18));
+
+        /* Создание таблицы с двумя столбцами */
+        var table = new Table(UnitValue.CreatePercentArray([1, 2]))
+            .UseAllAvailableWidth();
+
+        /* Добавление заголовков столбцов */
+        table.AddHeaderCell("Идентификатор");
+        table.AddHeaderCell("Значение");
+
+        /* Добавление строк данных */
+        foreach (var pair in data)
         {
-            var pdfWriter = new PdfWriter(memoryStream);
-            var pdfDocument = new PdfDocument(pdfWriter);
-            var document = new Document(pdfDocument);
-            
-            document.Add(new Paragraph("Id-Value Table")
-                .SetTextAlignment(TextAlignment.CENTER)
-                .SetFontSize(18));
-
-            // Создание таблицы с двумя столбцами
-            var table = new Table(UnitValue.CreatePercentArray(new float[] { 1, 2 }))
-                .UseAllAvailableWidth();
-
-            // Добавление заголовков столбцов
-            table.AddHeaderCell("ID");
-            table.AddHeaderCell("Value");
-
-            // Добавление строк данных
-            foreach (var pair in data)
-            {
-                table.AddCell(pair.Key.ToString());
-                table.AddCell(pair.Value);
-            }
-
-            // Добавление таблицы в документ
-            document.Add(table);
-            document.Close();
-            
-            var fileName = "YourFile.pdf";
-            return Results.File(memoryStream.ToArray(), "application/pdf", fileName);
+            table.AddCell(pair.Key.ToString());
+            table.AddCell(pair.Value);
         }
+
+        /* Добавление таблицы в документ */
+        document.Add(table);
+        document.Close();
+
+        var fileName = "Отчет.pdf";
+
+        return Results.File(memoryStream.ToArray(), "application/pdf", fileName);
     }
 }
